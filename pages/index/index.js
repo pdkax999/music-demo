5fbbe4ad7113c49afb265236958698cbb9939a37
dream <10683954@qq.com>
2021-08-13 09:59:27 +0800

    小程序首页

---
+++ pages/index/index.js
@@ -0,0 +1,87 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banners:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+    wx.request({
+      url:'http://localhost:3000/banner',
+      data:{
+        type:1
+      },
+      success:(res)=>{
+
+       this.setData({
+        banners:res.data.banners
+       })
+        
+
+      },
+      fail:(err)=> {
+
+        console.log(err);
+        
+
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
